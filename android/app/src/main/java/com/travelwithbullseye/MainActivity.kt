c493836c0bcc29be51f953bb164e35b7faf444fa
olya <olya0665643972@gmail.com>
2026-03-21 14:32:51 +0200

    init

--- android/app/src/main/java/com/travelwithbullseye/MainActivity.kt
+++ android/app/src/main/java/com/travelwithbullseye/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.travel.bullseye.placeslog
+package com.travel.travelwithbullseye
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
--- android/app/src/main/java/com/travelwithbullseye/MainApplication.kt
+++ android/app/src/main/java/com/travelwithbullseye/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.travel.bullseye.placeslog
+package com.travelwithbullseye
 
 import android.app.Application
 import com.facebook.react.PackageList
